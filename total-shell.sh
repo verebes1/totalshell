82197667ed0d89496c918040e12814aad22d3324
David <dverbenyi@gmail.com>
2023-11-15 15:58:54 +0000

    Add path fix after updating zsh for syntax
    -Added a step that adds the relevant PATH variable from bash to zsh

--- total-shell.sh
+++ total-shell.sh
@@ -8,6 +8,8 @@ if [[ $platform == 'Linux' ]]; then
   echo "Linux detected";
   sudo apt install -y zsh
   chsh -s $(which zsh)
+  echo 'export PATH="'$PATH':$PATH"' >> $HOME/.zshrc
+  sudo cat $HOME/.zshrc >> /root/.zshrc
 
 elif [[ $platform == 'Darwin' ]]; then
   echo "macOS detected";
@@ -15,6 +17,7 @@ elif [[ $platform == 'Darwin' ]]; then
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+cat $HOME/.zshrc.pre-oh-my-zsh >> $HOME/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
