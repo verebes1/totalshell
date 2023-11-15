d8a895c1f333b16a5bbd123988722a67a6ae9fd4
David <dverbenyi@gmail.com>
2023-11-15 15:44:54 +0000

    Update script with correct flag for sed
    -Updated the sed command flag to cater for macOS

--- total-shell.sh
+++ total-shell.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 platform=$(uname)
+theme="s|robbyrussell|powerlevel10k/powerlevel10k|"
+plugins='s|^plugins=(git)$|plugins=(git ruby colorize zsh-autosuggestions zsh-syntax-highlighting)|'
+flag="-i"
 
 if [[ $platform == 'Linux' ]]; then
   echo "Linux detected";
   sudo apt install -y zsh
+  chsh -s $(which zsh)
+
 elif [[ $platform == 'Darwin' ]]; then
-   echo "macOS detected";
+  echo "macOS detected";
+  flag="-i ''"
 fi
 
-chsh -s $(which zsh)
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-sed -i "s|robbyrussell|powerlevel10k/powerlevel10k|" ~/.zshrc;
-sed -i "s|^plugins=(git)$|plugins=(git ruby colorize zsh-autosuggestions zsh-syntax-highlighting)|" ~/.zshrc;
+sed $flag "$theme" $HOME/.zshrc;
+sed $flag "$plugins" $HOME/.zshrc;
